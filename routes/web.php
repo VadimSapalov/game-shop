8192576107a0cd62107aac6d63ac397e204fc604
Tori_II <vadimsapalev@gmail.com>
2026-02-22 22:17:17 +0200

    basic controllers created and routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\GamesController;
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MainController::class, 'show_main_page']);
+
+Route::get('/games', [GamesController::class, 'show_all_games']);
