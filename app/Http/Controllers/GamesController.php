a6b8621128997f7a2ceafe57f9ececabaffac7bf
Tori_II <vadimsapalev@gmail.com>
2026-02-23 14:19:04 +0200

    update GameController with 2 functions

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class GamesController extends Controller
 {
-    //
+    public function show_all_games() {
+        return view('games_list');
+    }
+
+    public function show_chosen_game($game_id) {
+        return view('game_by_id', ['id' => $game_id]);
+    }
 }
+
