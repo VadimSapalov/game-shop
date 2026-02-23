a7f45a766163678717d3d9017b832071ce3fd4db
Tori_II <vadimsapalev@gmail.com>
2026-02-23 14:18:12 +0200

    new routes added

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'show_main_page']);
 
+Route::get('/about', [MainController::class, 'show_about_page']);
+
 Route::get('/games', [GamesController::class, 'show_all_games']);
+
+Route::get('/games/{game_id}', [GamesController::class, 'show_chosen_game']) -> where('game_id', '[0-9]+');
