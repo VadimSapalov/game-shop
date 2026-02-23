db88df81c289a30cfefd36c370413043e31f84b2
Tori_II <vadimsapalev@gmail.com>
2026-02-23 14:19:25 +0200

    update MainController with about page

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,4 +9,8 @@ class MainController extends Controller
     public function show_main_page() {
         return ('Main page');
     }
+
+    public function show_about_page() {
+        return ('<h1>About</h1>');
+    }
 }
