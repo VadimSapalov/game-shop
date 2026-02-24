5c26b3f1b3730eebe11941995fe1b9d4dab44ba4
Tori_II <vadimsapalev@gmail.com>
2026-02-24 21:52:39 +0200

    Create card.blade.php

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,7 @@
+<div class="card w-100" style="width: 18rem; margin-bottom: 20px;">
+    <div class="card-body">
+        <h5 class="card-title">{{ $title }}</h5>
+        <p class="card-text">Publisher: {{ $publisher }}</p>
+        <a href="#" class="btn btn-primary">Детальніше</a>
+    </div>
+</div>
