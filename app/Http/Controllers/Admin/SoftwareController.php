37b07ac83148b99998fb6ad446f1f341a2ee1b3a
Tori_II <vadimsapalev@gmail.com>
2026-03-10 15:13:28 +0200

    update software controller
    add new methods for create page and function for storing new element to DB

--- app/Http/Controllers/Admin/SoftwareController.php
+++ app/Http/Controllers/Admin/SoftwareController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 
 class SoftwareController extends Controller
 {
+    //Метод для виведення списку всіх елементів таблиці software
     public function index()
     {
         $softwares = Software::all();
@@ -15,16 +16,41 @@ class SoftwareController extends Controller
         return view('admin.software.index', ['softwares' => $softwares]);
     }
 
+    //Метод для відкриття сторінки окремого елемента
     public function show(Software $software)
     {
         return view('admin.software.show', compact('software'));
     }
 
+    //Метод для видалення елементу з бази даних
     public function destroy(Software $software)
     {
         $software->delete();
 
         return redirect()->route('admin.software.index')
                          ->with('success', 'Програму видалено');
+    }
+
+    //Метод викликання сторінки створення
+    public function create()
+    {
+        return view('admin.software.create');
+    }
+
+    //Метод збереження створеного елементу в БД
+    public function store(Request $request)
+    {
+        //Валідація даних
+        $validated = $request->validate([
+            'Title' => 'required|string|max:255',
+            'Description' => 'required|min:10',
+            'Price' => 'required|numeric|min:0.01',
+            'ReleaseDate' => 'required|date',
+        ]);
+
+        Software::create($validated);
+
+        return redirect()->route('admin.software.index')
+                        ->with('success', 'Програму успішно додано до каталогу');
     }
 }
