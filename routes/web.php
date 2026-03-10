3e3ba6b4bfda64ae3ba58a971a60b545094effba
Tori_II <vadimsapalev@gmail.com>
2026-03-10 15:11:51 +0200

    New comment

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [SoftwareController::class, 'index'])->name('admin.software.index');
 
+//Створення посилань на сторінки методів в Software контролері
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::resource('software', SoftwareController::class);
 });
