bb2b9fb42041d12b08e28b26c03614dd21b784a3
Tori_II <vadimsapalev@gmail.com>
2026-03-18 21:08:03 +0200

    update app.blade.php for Laravel Breeze

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,36 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'My Game-Shop')</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css", rel="stylesheet">
-</head>
-<body class="d-flex flex-column min-vh-100">
-    <nav class="navbar bg-primary">
-        <div class="container">
-            <a class="navbar-brand text-light" href="/">Game Shop</a>
-            <a class="nav-link active text-light" aria-current="page" href="/about">About</a>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
+
+            <!-- Page Heading -->
+            @isset($header)
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endisset
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
         </div>
-    </nav>
-    <main class="flex-grow-1 container">
-        @yield('content')
-    </main>
-    <footer class="text-center border-top bottom">
-        <p>&copy; 2026 Всі права захищені | Сапальов В.С</p>
-    </footer>
-</body>
+    </body>
 </html>
