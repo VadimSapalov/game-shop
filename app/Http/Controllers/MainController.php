9f577b2acaaedbf423b7b49c711c443a08c87457
Tori_II <vadimsapalev@gmail.com>
2026-04-29 21:01:28 +0300

    Update MainController.php
    moved show func here and created library func

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,11 +2,53 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Software;
+use App\Models\Transaction;
 
 class MainController extends Controller
 {
-    public function about() {
-        return ('<h1>About</h1>');
+    public function show(Software $software)
+    {
+        $isOwned = false;
+
+        if (auth()->check()) {
+            $isOwned = Transaction::where('UserID', auth()->id())
+                ->where('SoftwareID', $software->id)
+                ->where('TransactionType', 'Purchase')
+                ->where('Status', 'Successful')
+                ->whereNotIn('TransactionID', function ($query) {
+                    $query->select('ParentTransactionID')
+                        ->from('Transactions')
+                        ->where('TransactionType', 'Refund')
+                        ->where('Status', 'Successful')
+                        ->whereNotNull('ParentTransactionID');
+                })
+                ->exists();
+        }
+
+        return view('show', compact('software', 'isOwned'));
+    }
+
+    public function library()
+    {
+        $userId = auth()->id();
+
+        //Запит на отримання списку транзакцій користувача без refund, для формування бібліотеки 
+        $softwares = Software::whereIn('id', function ($query) use ($userId) {
+            $query->select('SoftwareID')
+                ->from('Transactions')
+                ->where('UserID', $userId)
+                ->where('TransactionType', 'Purchase')
+                ->where('Status', 'Successful')
+                ->whereNotIn('TransactionID', function ($subquery) {
+                    $subquery->select('ParentTransactionID')
+                        ->from('Transactions')
+                        ->where('TransactionType', 'Refund')
+                        ->where('Status', 'Successful')
+                        ->whereNotNull('ParentTransactionID');
+                });
+        })->get();
+
+        return view('library', compact('softwares'));
     }
 }
