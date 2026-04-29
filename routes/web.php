738b9f5230211ddae2e80e269a79c87c21194f11
Tori_II <vadimsapalev@gmail.com>
2026-04-29 21:03:28 +0300

    Update web.php
    new routes for show and purchase, new is_admin check routes and library route

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,24 @@
 <?php
 
 use App\Http\Controllers\Admin\SoftwareController;
+use App\Http\Controllers\MainController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [SoftwareController::class, 'home'])->name('home');
+Route::get('/software/{software}', [MainController::class, 'show'])->name('show');
+Route::post('/software/{software}/purchase', [SoftwareController::class, 'purchase'])->name('software.purchase');
 
 // Захищені маршрути адміністрування
-Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
+Route::middleware(['auth', 'verified', 'is_admin'])->prefix('admin')->name('admin.')->group(function () {
     Route::resource('software', SoftwareController::class);
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/library', [MainController::class, 'library'])->name('library');
 });
 
 require __DIR__.'/auth.php';
