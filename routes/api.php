8151f99a2f29d5b7569d3be575997294ebefa3e5
Tori_II <vadimsapalev@gmail.com>
2026-05-10 01:14:49 +0300

    + genre

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
 use App\Http\Controllers\Api\SoftwareController;
+use App\Models\Genre;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResources ([
     'software'=> SoftwareController::class
     ]);
+Route::get('/genres', function () {
+    return response()->json([
+        'data' => Genre::all()
+    ]);
+});
