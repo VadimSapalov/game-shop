29b727e74b07ed2a6b0d74eae106ac3896686ee4
Tori_II <vadimsapalev@gmail.com>
2026-05-10 01:13:03 +0300

    Create GenreSeeder.php

---
+++ database/seeders/GenreSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class GenreSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $genres = ['Action', 'RPG', 'Strategy', 'Simulation', 'Horror'];
+
+        foreach ($genres as $genre) {
+            \App\Models\Genre::create(['name' => $genre]);
+        }
+    }
+}
